c7e7abfb308bb0d3a4c5b25a6584cc713779e8c3
alkon <aikbox@yahoo.com>
2025-06-06 09:09:33 +0300

    Set app to run as web server with Flask to prevent short-running app process

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-print("Hello, world!")
+# Example app/main.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world_route():
+    return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
